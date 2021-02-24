14b57a390243da63c8357f884448ede2911b452d
Petr Belskiy <petr.belskiy@gmail.com>
2021-02-24 11:21:58 +0300

    LC34: add Python solution
    Runtime: 76 ms, faster than 97.40% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.
    Memory Usage: 15.3 MB, less than 82.68% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.

---
+++ leetcode.com/0034_find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -0,0 +1,4 @@
+LC34: add Python solution
+
+Runtime: 76 ms, faster than 97.40% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.
+Memory Usage: 15.3 MB, less than 82.68% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.

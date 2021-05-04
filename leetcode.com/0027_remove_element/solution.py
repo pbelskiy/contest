b141691d3e4452d1bec670844487f029d926eee6
Petr Belskiy <petr.belskiy@gmail.com>
2021-05-04 15:43:41 +0300

    LC27: add Python solution

---
+++ leetcode.com/0027_remove_element/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        p = 0
+
+        for i in range(len(nums)):
+            if nums[i] == val:
+                continue
+
+            nums[p] = nums[i]
+            p += 1
+
+        return p

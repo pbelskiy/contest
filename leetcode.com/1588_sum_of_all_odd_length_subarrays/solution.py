1d9c5e3963b358a2dfc5405fa1a3e4271b8cd38a
Petr Belskiy <petr.belskiy@gmail.com>
2021-10-17 00:50:49 +0300

    LC1588: add Python solution

---
+++ leetcode.com/1588_sum_of_all_odd_length_subarrays/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        total = 0
+
+        for i in range(1, len(arr) + 1, 2):
+            for j in range(len(arr) - i + 1):
+                total += sum(arr[j:j+i])
+
+        return total

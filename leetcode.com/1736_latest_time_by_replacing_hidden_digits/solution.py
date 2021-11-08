316aa2fb6f3313509ece0b0c63b24a018b7fab33
Petr Belskiy <petr.belskiy@gmail.com>
2021-11-09 00:38:30 +0300

    LC1736: add Python solution

---
+++ leetcode.com/1736_latest_time_by_replacing_hidden_digits/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        time = list(time)
+
+        if time[0] == '?':
+            time[0] = '2' if time[1] in ('?', '0', '1', '2', '3') else '1'
+
+        if time[1] == '?':
+            time[1] = '3' if time[0] == '2' else '9'
+
+        if time[3] == '?':
+            time[3] = '5'
+
+        if time[4] == '?':
+            time[4] = '9'
+
+        return ''.join(time)

9ef30d005e7f5e297c263bd3ac2aa1fc41953d03
Petr Belskiy <petr.belskiy@gmail.com>
2022-02-17 02:14:16 +0300

    LC1038: add Python solution

---
+++ leetcode.com/1038_binary_search_tree_to_greater_sum_tree/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+
+        def dfs_get(node):
+            if not node:
+                return
+            nums.append(node.val)
+            dfs_get(node.left)
+            dfs_get(node.right)
+
+        def dfs_set(node):
+            if not node:
+                return
+
+            node.val += s[node.val]
+
+            dfs_set(node.left)
+            dfs_set(node.right)
+
+        nums = []
+        dfs_get(root)
+        nums.sort()
+
+        s = {}
+
+        for i, n in enumerate(nums):
+            s[n] = sum(nums[i+1:])
+
+        dfs_set(root)
+        return root

d654447bb68d265b5981be5d67cf40e3f9876140
Petr Belskiy <petr.belskiy@gmail.com>
2022-03-25 00:28:13 +0300

    LC1346: add Python solution

---
+++ leetcode.com/1346_check_if_n_and_its_double_exist/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if i != j and arr[i] == arr[j]*2:
+                    return True
+
+        return False

be5bb2a2eee54b2df4de4c2999294693c4299f5f
Petr Belskiy <petr.belskiy@gmail.com>
2022-04-07 00:04:55 +0300

    LC48: add Python solution

---
+++ leetcode.com/0048_rotate_image/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+
+        def reverse_diagonal(y1, x1):
+            y2 = len(matrix) - 1 - x1
+            x2 = len(matrix) - 1 - y1
+
+            while y1 < y2:
+                matrix[y1][x1], matrix[y2][x2] = matrix[y2][x2], matrix[y1][x1]
+                y1, x1 = y1 + 1, x1 + 1
+                y2, x2 = y2 - 1, x2 - 1
+
+        for row in matrix:
+            row.reverse()
+
+        # reverse left-right diagonals
+        for i in range(len(matrix[0]) - 2, -1, -1):
+            reverse_diagonal(0, i)
+
+        for i in range(1, len(matrix)):
+            reverse_diagonal(i, 0)

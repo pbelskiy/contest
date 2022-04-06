577b2a148feb6c484710aa5480f8e3ec5123559b
Petr Belskiy <petr.belskiy@gmail.com>
2022-04-07 00:04:10 +0300

    LC69: add new Python solution

--- leetcode.com/0069_sqrt_x/solution.py
+++ leetcode.com/0069_sqrt_x/solution.py
@@ -2,3 +2,24 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         import math
         return int(math.sqrt(x))
+
+   def mySqrt2(self, x: int) -> int:
+        if x == 1:
+            return 1
+
+        lo, hi = 0, x // 2
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+
+            s = mid * mid
+
+            if s == x:
+                return mid
+
+            if s < x:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        return hi

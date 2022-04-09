7b5515e65b3680d9d510a52c561dc7e3d8b383d0
Petr Belskiy <petr.belskiy@gmail.com>
2022-04-10 00:03:01 +0300

    LC703: add Python solution

---
+++ leetcode.com/0703_kth_largest_element_in_a_stream/solution.py
@@ -0,0 +1,10 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        self.k = k
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort(reverse=True)
+        return self.nums[self.k - 1]

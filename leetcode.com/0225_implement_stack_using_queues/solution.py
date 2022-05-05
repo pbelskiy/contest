d7d6d9046769e6a5adec7e270d1cc8d2d6404c8e
Petr Belskiy <petr.belskiy@gmail.com>
2022-05-06 00:03:09 +0300

    LC225: add Python solution

---
+++ leetcode.com/0225_implement_stack_using_queues/solution.py
@@ -0,0 +1,16 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        return self.q.pop()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0

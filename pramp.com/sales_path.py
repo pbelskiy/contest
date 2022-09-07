00eb0aed3f1a0d002c5b55b5fc002d86ec4f2821
Petr Belskiy <petr.belskiy@gmail.com>
2022-09-08 00:24:52 +0300

    add new Pramp solution

---
+++ pramp.com/sales_path.py
@@ -0,0 +1,28 @@
+def get_cheapest_cost(rootNode):
+
+  def dfs(node, total):
+    if not rootNode:
+      return 0
+
+    best = float('inf')
+    for child in rootNode.children:
+      best = min(best, total + rootNode.val + dfs(child))
+
+    return best
+
+  return dfs(rootNode, 0)
+
+
+##########################################
+# Use the helper code below to implement #
+# and test your function above           #
+##########################################
+
+# A node
+class Node:
+
+  # Constructor to create a new node
+  def __init__(self, cost):
+    self.cost = cost
+    self.children = []
+    self.parent = None

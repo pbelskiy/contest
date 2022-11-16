0ae50e5d9bb7dfdfeb240f391ac66d7fcb59b723
Petr Belskiy <petr.belskiy@gmail.com>
2022-11-17 01:06:52 +0300

    LC2468: update solution due new TC

--- leetcode.com/2468_split_message_based_on_limit/solution.py
+++ leetcode.com/2468_split_message_based_on_limit/solution.py
@@ -9,20 +9,26 @@ class Solution:
         # calc start cap
         while True:
             chunks = len(message) / (limit - length)
-            if chunks < cap:
+            if chunks <= cap:
                 break
             cap = int(str(cap) + '9')
 
         # count total chunks
-        j = 0
-        total = 0
+        while True:
+            j = 0
+            total = 0
 
-        while j < len(message):
-            curr_chunk_len = limit - (1 + len(str(total + 1)) + 1 + len(str(cap)) + 1)
-            j += curr_chunk_len
-            total += 1
-            if curr_chunk_len <= 0:
-                return []
+            while j < len(message):
+                curr_chunk_len = limit - (1 + len(str(total + 1)) + 1 + len(str(cap)) + 1)
+                j += curr_chunk_len
+                total += 1
+
+                if curr_chunk_len <= 0:
+                    return []
+
+            if total <= cap:
+                break
+            cap = total
 
         # format chunks
         i = 1

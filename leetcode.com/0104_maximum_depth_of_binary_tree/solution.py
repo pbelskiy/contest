ca6ff6e9b73fd91b055b18c96d71dfca230299b1
Petr Belskiy <petr.belskiy@gmail.com>
2023-02-16 09:48:56 +0300

    LC104: add new Python solution

--- leetcode.com/0104_maximum_depth_of_binary_tree/solution.py
+++ leetcode.com/0104_maximum_depth_of_binary_tree/solution.py
@@ -8,3 +8,15 @@ class Solution:
 
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         return self.dfs(root, 0)
+
+
+# 16 Feb 2023
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node, d):
+            if not node:
+                return d
+            return max(dfs(node.left, d + 1), dfs(node.right, d + 1))
+
+        return dfs(root, 0)

f882e6b6759de20fe3df128db041946bfebfbacf
Petr Belskiy <petr.belskiy@gmail.com>
2023-03-04 20:47:07 +0300

    LC2579: add comments + O(1) solution

--- leetcode.com/2579_count_total_number_of_colored_cells/solution.py
+++ leetcode.com/2579_count_total_number_of_colored_cells/solution.py
@@ -1,3 +1,15 @@
+"""
+Naive approach, we could see that is just +4 new block on each step.
+
+brute force solution is O(N):
+    return 1 + 4*sum(range(n))
+
+But it's easy to translate it to O(1):
+    return 1 + 4*(n*(n - 1) // 2)
+
+https://en.wikipedia.org/wiki/Arithmetic_progression
+"""
+
 class Solution:
     def coloredCells(self, n: int) -> int:
-        return 1 + 4*sum(range(n))
+        return 1 + 4*(n*(n - 1) // 2)

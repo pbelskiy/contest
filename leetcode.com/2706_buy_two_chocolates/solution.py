596410a54bc43b0224f45fc372dbf0e30f646b13
Petr Belskiy <petr.belskiy@gmail.com>
2023-05-27 19:46:19 +0300

    LC2706: add Python solution

---
+++ leetcode.com/2706_buy_two_chocolates/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        s = prices[0] + prices[1]
+
+        if money >= s:
+            return money - s
+
+        return money

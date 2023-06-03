c9f3aec91ddc1cd8593d83c1addf53f5d752f455
Petr Belskiy <petr.belskiy@gmail.com>
2023-06-03 23:47:47 +0300

    LC865: add Python solution

---
+++ leetcode.com/0865_smallest_subtree_with_all_the_deepest_nodes/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        # collect layers
+        q = [(root, None)]
+        while q:
+            nq = []
+            latest = q[:]
+            while q:
+                n, p = q.pop()
+                n.p = p
+
+                if n.left:
+                    nq.append((n.left, n))
+                if n.right:
+                    nq.append((n.right, n))
+
+            q = nq
+
+        # find near common parent
+        if len(latest) == 1:
+            return latest[0][0]
+
+        parents = [n[1] for n in latest]
+        while len(set(parents)) > 1:
+            parents = [n.p for n in parents]
+
+        return parents[0]

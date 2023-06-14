5631a294979a9e26ba8fe5b1145baef04c9ab397
Petr Belskiy <petr.belskiy@gmail.com>
2023-06-15 01:56:18 +0300

    LC1146: update solution due to new TCs

--- leetcode.com/1146_snapshot_array/solution.py
+++ leetcode.com/1146_snapshot_array/solution.py
@@ -1,15 +1,34 @@
 class SnapshotArray:
+    MOD = 100
 
     def __init__(self, length: int):
-        self.curr = collections.defaultdict(int)
+        self.curr = defaultdict(int)
         self.list = []
+        self.cache = {}
 
     def set(self, index: int, val: int) -> None:
         self.curr[index] = val
 
     def snap(self) -> int:
-        self.list.append(copy.deepcopy(self.curr))
-        return len(self.list) - 1
+        self.list.append(self.curr)
+        self.curr = defaultdict(int)
+        snap_id = len(self.list) - 1
+
+        return snap_id
 
     def get(self, index: int, snap_id: int) -> int:
-        return self.list[snap_id][index]
+        key = snap_id // self.MOD
+        if key in self.cache:
+            start = key * self.MOD
+            d = self.cache[key].copy()
+        else:
+            start = 0
+            d = defaultdict(int)
+
+        for i in range(start, snap_id + 1):
+            if i % self.MOD == 0 and i // self.MOD not in self.cache:
+                self.cache[i // self.MOD] = d.copy()
+
+            d |= self.list[i]
+
+        return d[index]

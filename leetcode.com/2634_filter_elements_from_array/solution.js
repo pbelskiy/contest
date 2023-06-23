931c2a2060bcd90c7ead5437e29efca25a92fee3
Petr Belskiy <petr.belskiy@gmail.com>
2023-06-23 15:05:07 +0300

    LC2634: add JS solution

---
+++ leetcode.com/2634_filter_elements_from_array/solution.js
@@ -0,0 +1,11 @@
+var filter = function(arr, fn) {
+    let newArr = [];
+
+    for (let i = 0 ; i < arr.length ; i++) {
+        if (fn(arr[i], i)) {
+            newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
+};

89ccef678e59d79655eb0927660d080127408a34
Petr Belskiy <petr.belskiy@gmail.com>
2023-09-16 23:14:39 +0300

    LC1410: add Python solution

---
+++ leetcode.com/1410_html_entity_parser/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def entityParser(self, text: str) -> str:
+        return (text.replace('&quot;', '"')
+                    .replace('&apos;',  "'")
+                    .replace('&gt;', '>')
+                    .replace('&lt;', '<')
+                    .replace('&frasl;', '/')
+                    .replace('&amp;', '&'))

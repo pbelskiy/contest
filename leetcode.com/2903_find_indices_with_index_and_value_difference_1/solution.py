e69514c752ff2ba1587f3e4841e8355f5076aa6e
Petr Belskiy <petr.belskiy@gmail.com>
2023-10-17 22:50:57 +0300

    LC2903

---
+++ leetcode.com/2903_find_indices_with_index_and_value_difference_1/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if abs(i - j) < indexDifference:
+                    continue
+
+                if abs(nums[i] - nums[j]) >= valueDifference:
+                    return [i, j]
+
+        return [-1, -1]

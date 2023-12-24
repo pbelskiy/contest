68ad535a6bc362fcb77f2fb2e2b34193cfc8b83d
Petr Belskiy <petr.belskiy@gmail.com>
2023-12-24 12:32:17 +0300

    LC2974: [easy] add Python solution

---
+++ leetcode.com/2974_minimum_number_game/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        arr = []
+
+        for i in range(0, len(nums), 2):
+            arr.append(nums[i + 1])
+            arr.append(nums[i])
+
+        return arr

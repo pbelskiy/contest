938764910c7d3c884f867c018685083878493f58
Petr Belskiy <petr.belskiy@gmail.com>
2024-05-26 20:43:52 +0300

    LC3162: [easy] add Python solution

---
+++ leetcode.com/3162_find_the_number_of_good_pairs_1/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        t = 0
+
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] % (nums2[j] * k) == 0:
+                    t += 1
+
+        return t

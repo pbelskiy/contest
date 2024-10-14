1b7c842d3fc4bb4d4daf6f96de04d1ad59e3c39a
Petr Belskiy <petr.belskiy@gmail.com>
2024-10-14 20:57:20 +0800

    LC3314: [easy] add Python solution

---
+++ leetcode.com/3314_construct_the_minimum_bitwise_array_1/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for n in nums:
+            for x in range(n):
+                if x | (x + 1) == n:
+                    ans.append(x)
+                    break
+            else:
+                ans.append(-1)
+
+        return ans

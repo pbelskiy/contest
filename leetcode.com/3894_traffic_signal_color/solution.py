9b2d9a219035defd4af4ef788a73193041bfc308
Petr Belskiy <petr.belskiy@gmail.com>
2026-04-14 00:34:55 +0300

    LC3894: [easy] add Python solution

---
+++ leetcode.com/3894_traffic_signal_color/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trafficSignal(self, timer: int) -> str:
+        if timer == 0:
+            return "Green"
+        if timer == 30:
+            return "Orange"
+        if 30 < timer <= 90:
+            return "Red"
+        
+        return "Invalid"
+
